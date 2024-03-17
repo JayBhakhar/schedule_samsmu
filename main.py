f7df8cb00401b189abca903324619d9239be3ee1
JayBhakhar <jaybhakhar@gmail.com>
2024-03-17 13:10:39 +0400

    Data Added

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import uvicorn
 from fastapi import FastAPI
 from flask_pymongo import MongoClient
+import pandas as pd
+import uuid
 
 app = FastAPI()
 MongoURL = "mongodb+srv://JayBhakhar:jay456789@schedulesamsmu.uczju06.mongodb.net/"
+SchduleSaMSMUCollection = MongoClient(MongoURL).datadase.SchduleSaMSMU
+
+def addData():
+    data = pd.read_excel(r'Book1.xlsx')
+    df = data[['Date','Time','Subject_name','Group_name','Address','Teacher_name','Room_no','type']] 
+    df['_id'] = 0
+    print(df['Date'])
+    for i in range(len(df['_id'])):
+        df.loc[i, '_id'] = str(uuid.uuid4())
+    df = df.where(pd.notnull(df), None)
+    df.replace({pd.NaT: None})
+    results = df.to_dict(orient="records")
+    SchduleSaMSMUCollection.insert_many(results)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    output = []
+    for supplier in SchduleSaMSMUCollection.find():
+        output.append(supplier)
+    return {'Schdule': output}
 
 
 if __name__ == "__main__":
